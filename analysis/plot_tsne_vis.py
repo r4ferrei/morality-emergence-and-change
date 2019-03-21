353cdf0eb7d0bbc62a3a4a8ef6808bf3bf8ee46a
jing <xie.j97@gmail.com>
2019-03-21 15:13:05 -0400

    Make density plots

--- analysis/plot_tsne_vis.py
+++ analysis/plot_tsne_vis.py
@@ -4,8 +4,29 @@ import models
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import re
+from sklearn.neighbors import KernelDensity
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import numpy as np
+import seaborn as sns; sns.set(color_codes=True)
+
+def use_matplot(mfd_dict):
+    cmap = plt.get_cmap('tab10')
+    base_cats = ['care', 'fairness', 'loyalty', 'authority', 'sanctity']
+    all_points = []
+    all_labels = []
+    for cat in mfd_dict[constant.CATEGORY].unique():
+        word_df = mfd_dict[mfd_dict[constant.CATEGORY] == cat]
+        base_cat = re.sub('[\+\-]+', '', cat)
+        x, y, color = word_df['x'].values.tolist(), word_df['y'].values.tolist(), cmap(base_cats.index(base_cat))
+        marker = 'v' if '-' in cat else 'o'
+        points = plt.scatter(x=x, y=y, marker=marker, color=color, alpha=0.7, s=100)
+        word_ind = 5
+        word = word_df[constant.WORD].values.tolist()[word_ind]
+        bbox_props = dict(boxstyle="square,pad=0.3", fc=color, alpha=0.2)
+        plt.text(x[word_ind],y[word_ind],word,size=15,bbox=bbox_props)
+        all_points.append(points)
+        all_labels.append(cat)
+    plt.legend(all_points, all_labels)
 
 load = False
 
@@ -23,21 +44,10 @@ tsne_results = tsne.fit_transform(list(mfd_dict[constant.VECTOR].values))
 mfd_dict['x'] = [pair[0] for pair in tsne_results]
 mfd_dict['y'] = [pair[1] for pair in tsne_results]
 
-cmap = plt.get_cmap('tab10')
-base_cats = ['care', 'fairness', 'loyalty', 'authority', 'sanctity']
-all_points = []
-all_labels = []
 for cat in mfd_dict[constant.CATEGORY].unique():
+    alpha = 0.5
     word_df = mfd_dict[mfd_dict[constant.CATEGORY] == cat]
-    base_cat = re.sub('[\+\-]+', '', cat)
-    x, y, color = word_df['x'].values.tolist(), word_df['y'].values.tolist(), cmap(base_cats.index(base_cat))
-    marker = 'v' if '-' in cat else 'o'
-    points = plt.scatter(x=x, y=y, marker=marker, color=color, alpha=0.7, s=100)
-    word_ind = 5
-    word = word_df[constant.WORD].values.tolist()[word_ind]
-    bbox_props = dict(boxstyle="square,pad=0.3", fc=color, alpha=0.2)
-    plt.text(x[word_ind],y[word_ind],word,size=15,bbox=bbox_props)
-    all_points.append(points)
-    all_labels.append(cat)
-plt.legend(all_points, all_labels)
+    sns.kdeplot(word_df['x'].values.tolist(), word_df['y'].values.tolist(), shade=True, shade_lowest=False,
+                cmap=constant.get_colour_map(cat), alpha=alpha)
+
 plt.show()
