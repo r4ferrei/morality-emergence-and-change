19c2c73cc122da9aeab15ac6605864a027f1aa93
xiejxie <xie.j97@gmail.com>
2019-05-21 11:57:24 -0400

    Update constant.py

--- src/constant.py
+++ src/constant.py
@@ -8,12 +8,24 @@ def get_colour(category_name):
     base_cats = ['care', 'fairness', 'loyalty', 'authority', 'sanctity']
     return cmap(base_cats.index(base_cat))
 
-TEMP_DATA_DIR = 'data'
-DATA_DIR = 'local-data'
-SGNS_DIR = 'E:/sgns'
+def get_linestyle(category_name):
+    linestyles = [(0,()), (0,(1,1)), (0,(5,1)), (0,(5,5)), (0,(3,5,1,5,1,5))]
+    base_cat = re.sub('[\+\-]+', '', category_name)
+    base_cats = ['care', 'fairness', 'loyalty', 'authority', 'sanctity']
+    return linestyles[base_cats.index(base_cat)]
+
+TEMP_DATA_DIR = 'C:/Users/Jing/Documents/GitHub/morality-emergence-and-change/data'
+DATA_DIR = 'C:/Users/Jing/Documents/GitHub/morality-emergence-and-change/local-data'
+# SGNS_DIR = 'D:/sgns'
+SGNS_COHA_DIR = 'C:/Users/Jing/Documents/GitHub/morality-emergence-and-change/data/coha-word_sgns/sgns'
+SGNS_DIR = 'C:/Users/Jing/Documents/GitHub/morality-emergence-and-change/data/sgns'
+SGNS_NYT_DIR = 'C:/Users/Jing/Documents/GitHub/morality-emergence-and-change/data/nyt'
+SGNS_FICTION_DIR = 'D:/kim'
 
 DICTIONARY_PATH = os.path.join(DATA_DIR, 'dictionary.txt')
-MFD_DF = os.path.join(DATA_DIR, 'mfd_dic.pkl')
+MFD_DF = os.path.join(TEMP_DATA_DIR, 'mfd_dic.pkl')
+MFD_DF_NEUTRAL = os.path.join(TEMP_DATA_DIR, 'mfd_dic_neutral.pkl')
+MFD_DF_BINARY = os.path.join(TEMP_DATA_DIR, 'mfd_dic_binary.pkl')
 
 CATEGORY = 'category'
 WORD = 'word'
